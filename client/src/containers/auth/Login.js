a55dd8ba296586e4f0978f9b8c124ca54eabae12
prakash1126 <prahlad1126@gmail.com>
2023-01-04 17:37:08 +0545

    login and navigate function added in UI and backend validation

--- client/src/containers/auth/Login.js
+++ client/src/containers/auth/Login.js
@@ -6,11 +6,9 @@ import img from "../../image/login.png";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
-
 const Login = () => {
-
     const usersSchema = Yup.object().shape({
-        username: Yup.string()
+        email: Yup.string()
             .min(5, "Too Short!")
             .max(100, "Too Long!")
             .required("Required"),
@@ -30,50 +28,55 @@ const Login = () => {
                         <h3>Welcome to login page</h3>
                         <Formik
                             initialValues={{
-                                username: "",
+                                email: "",
                                 password: "",
 
                             }}
                             validationSchema={usersSchema}
-                            onSubmit= { async(values) => {
+                            onSubmit={async (values) => {
                                 const requestOptions = {
                                     method: "POST",
                                     headers: { "Content-Type": "application/json" },
                                     body: JSON.stringify(values),
                                 };
-                                try{
-                                    const response= await fetch("http://localhost:3005/login", requestOptions)
+                                try {
+                                    const res = await fetch("http://localhost:3005/login", requestOptions)
                                     console.log(response);
-                                    const data= await response.json();
-                                    console.log(data)
-                                    // navigate("/rider");
+                                    const data = await res.json()
+                                    if (res.status = 200) {
+                                        alert(data.msg)
+                                        navigate('/rider')
+                                    }
+                                    else {
+                                        alert(data.msg)
+                                    }
                                 }
-                                catch(err){
+                                catch (err) {
                                     alert(err)
                                 }
-                                
+
                             }}
                         >
 
                             {({ errors, touched }) => (
-                                <div style={{display:"flex",alignItem:'center',justifyContent:'center'}}>
-                                <Form >
+                                <div style={{ display: "flex", alignItem: 'center', justifyContent: 'center' }}>
+                                    <Form >
 
-                                    <div>
-                                        <Field name="username" placeholder="Username " />
-                                        {errors.username && touched.username ? (
-                                                <div className="validaton-message">{errors.username}</div>
-                                        ) : null}
-                                    </div>
-                                    <div>
-                                        <Field name="password" placeholder="Password" type="password" />
-                                        {errors.password && touched.password ? (
+                                        <div>
+                                            <Field name="email" placeholder="email " />
+                                            {errors.email && touched.email ? (
+                                                <div className="validaton-message">{errors.email}</div>
+                                            ) : null}
+                                        </div>
+                                        <div>
+                                            <Field name="password" placeholder="Password" type="password" />
+                                            {errors.password && touched.password ? (
                                                 <div className="validaton-message">{errors.password}</div>
-                                        ) : null}
-                                    </div>
+                                            ) : null}
+                                        </div>
 
-                                    <Button name='Submit' type="submit" />
-                                </Form>
+                                        <Button name='Submit' type="submit" />
+                                    </Form>
                                 </div>
                             )}
                         </Formik>
@@ -81,7 +84,7 @@ const Login = () => {
                     <div className="right-side">
 
                         <div className="img-box">
-                            <img src={img} alt="Logo"width={300}/>
+                            <img src={img} alt="Logo" width={300} />
                             <div className="">
                                 <span><Link to='/register'>Create an account </Link></span>
                             </div>
