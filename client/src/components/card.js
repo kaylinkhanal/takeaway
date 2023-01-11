6705210f06cbc0618ccf949e9bb879f281e2779d
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-01-11 17:54:06 +0545

    fixed git issues

---
+++ client/src/components/card.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import './button.css'
+import { Drawer, Modal, Button } from "antd";
+import CustomForm from "../components/customForm"
+ const Card = (props) => {
+ const [isModalOpen, setIsModalOpen] = useState(false)
+  return (
+      <>
+      <Modal 
+      footer={null}
+      onCancel={()=>setIsModalOpen(false)}
+      open={isModalOpen}>
+         {props.item.catagoryName}
+         <CustomForm/>
+      </Modal>
+    <div onClick={()=> setIsModalOpen(true)}style={{padding:'30px', backgroundColor:'pink',width:'100px', margin:'10px'}}>
+       {props.item.catagoryName}
+    </div>
+    </>
+  )
+}
+export default Card
