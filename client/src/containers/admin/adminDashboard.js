b30288d606f659084d31aada0ad5298a64003694
hari-basnet <harikrishnabsnt@gmail.com>
2023-01-19 14:00:10 +0200

    Handle form close onsubmit

--- client/src/containers/admin/adminDashboard.js
+++ client/src/containers/admin/adminDashboard.js
@@ -1,60 +1,56 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Drawer, Modal, Button,Table } from "antd";
-import {faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Drawer, Modal, Button, Table } from "antd";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../App.css";
 
 import { Link } from "react-router-dom";
-import * as Yup from 'yup';
-import {useDispatch} from "react-redux";
-import ReusableForm from '../../components/reusableForm'
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import ReusableForm from "../../components/reusableForm";
 // import {logoutResetDetails} from "../../redux/actions/userAction"
 
 const AdminDashboard = () => {
-  const [orders, setOrders]= useState([])
-  const [columns, setColumns]=useState([
+  const [orders, setOrders] = useState([]);
+  const [columns, setColumns] = useState([
     {
-      title: 'Pickup Date',
-      dataIndex: 'pickupDate',
+      title: "Pickup Date",
+      dataIndex: "pickupDate",
     },
     {
-      title: 'Pickup Time',
-      dataIndex: 'pickupTime',
+      title: "Pickup Time",
+      dataIndex: "pickupTime",
     },
     {
-      title: 'Reciver Name',
-      dataIndex: "receiverName"
+      title: "Reciver Name",
+      dataIndex: "receiverName",
     },
     {
-      title: 'Phone Number',
+      title: "Phone Number",
       dataIndex: "receiverPhoneNo",
     },
     {
-      title: 'Unit Items',
-      dataIndex: 'unitItems',
+      title: "Unit Items",
+      dataIndex: "unitItems",
     },
     {
-      title: 'Weight',
-      dataIndex: 'weight',
+      title: "Weight",
+      dataIndex: "weight",
     },
     {
-      title: 'Actions',
-      key: 'key',
-      dataIndex: 'key',
+      title: "Actions",
+      key: "key",
+      dataIndex: "key",
       render: () => (
         <>
-        <Button>
-         {'Accept'}
-       </Button>
-       <Button>
-         {'Delete'}
-       </Button>
+          <Button>{"Accept"}</Button>
+          <Button>{"Delete"}</Button>
         </>
       ),
     },
-  ])
-  const dispatch= useDispatch()
+  ]);
+  const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
@@ -71,16 +67,14 @@ const AdminDashboard = () => {
   const onClose = () => {
     setOpen(false);
   };
-  const fetchAvailableItems= ()=>{
+  const fetchAvailableItems = () => {
     axios.get(`${process.env.REACT_APP_API_URL}/orders`).then((response) => {
-        setOrders(response.data.orders)
-      });
-}
-useEffect(()=>{
-    fetchAvailableItems()
-}, [])
-
-
+      setOrders(response.data.orders);
+    });
+  };
+  useEffect(() => {
+    fetchAvailableItems();
+  }, []);
 
   // const triggerLogout = () => {
   //   dispatch(logoutResetDetails())
@@ -88,15 +82,21 @@ useEffect(()=>{
 
   return (
     <>
-      <FontAwesomeIcon icon={faBars}  onClick={showDrawer}className="adminDrawer"></FontAwesomeIcon>
-      <Button onClick={showModal} className="addItems">Add Items</Button>
+      <FontAwesomeIcon
+        icon={faBars}
+        onClick={showDrawer}
+        className="adminDrawer"
+      ></FontAwesomeIcon>
+      <Button onClick={showModal} className="addItems">
+        Add Items
+      </Button>
       <Modal
         title="Add Items"
         footer={null}
         open={isModalOpen}
         onCancel={handleCancel}
       >
-        <ReusableForm/>
+        <ReusableForm handleCancel={handleCancel} />
       </Modal>
       <Drawer
         title="Admin options"
@@ -104,12 +104,14 @@ useEffect(()=>{
         onClose={onClose}
         open={open}
       >
-        <Link to="/items"><li>Delivery Items</li></Link>
+        <Link to="/items">
+          <li>Delivery Items</li>
+        </Link>
         <li>Update crendentials</li>
       </Drawer>
-   
+
       <div>
-      <Table dataSource={orders} columns={columns} />;
+        <Table dataSource={orders} columns={columns} />;
       </div>
     </>
   );
