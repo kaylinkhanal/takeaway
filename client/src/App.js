8a1a37650da13b4a0aff3b01bc32dd968450da47
hari-basnet <harikrishnabsnt@gmail.com>
2023-01-21 15:48:46 +0200

    Not found page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
-import {  Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Register from "./containers/auth/register";
 import Login from "./containers/auth/login";
 import UserDashboard from "./containers/user/userDashboard";
 import RiderDashboard from "./containers/rider/riderDashboard";
 import AdminDashboard from "./containers/admin/adminDashboard";
 import NavBar from "./components/navBar";
-import Items from "./containers/sharedScreens/items"
+import Items from "./containers/sharedScreens/items";
 import { useSelector } from "react-redux";
 import Profile from "./containers/sharedScreens/profile";
+import NotFoundPage from "./components/notFoundPage";
 
 function App() {
   const {role , token} =useSelector(state=>state.user)
@@ -26,38 +27,39 @@ function App() {
 const AuthScreens = () => {
   return (
     <Routes>
-    <Route path="/register" element={<Register />} />
-    <Route path="/" element={<Login />} />
-  </Routes>
-  )
-}
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
+};
 
 const AdminScreens = () => {
   return (
     <Routes>
-    <Route exact path="/" element={<AdminDashboard />} />
-    <Route exact path="/items" element={<Items />} />
-    <Route exact path="/profile" element={<Profile />} />
-
+      <Route path="*" element={<NotFoundPage />} />
+      <Route exact path="/" element={<AdminDashboard />} />
+      <Route exact path="/items" element={<Items />} />
+      <Route exact path="/profile" element={<Profile />} />
     </Routes>
-  )
-}
+  );
+};
 
 const UserScreens = () => {
   return (
     <Routes>
-    <Route exact path="/" element={<UserDashboard />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route exact path="/" element={<UserDashboard />} />
     </Routes>
-  )
-}
+  );
+};
 
 const RiderScreens = () => {
   return (
     <Routes>
-    <Route exact path="/" element={<RiderDashboard/>} />
-  </Routes>
-  )
-}
-export default App
-
-
+      <Route path="*" element={<NotFoundPage />} />
+      <Route exact path="/" element={<RiderDashboard />} />
+    </Routes>
+  );
+};
+export default App;
