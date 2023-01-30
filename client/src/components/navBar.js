9877b46b2edc6468b86e5e20d33d42c90591e360
SahilDeula <sahildeula333@gmail.com>
2023-01-30 22:12:23 +0545

    added logo and changes in nav bar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -3,12 +3,12 @@ import { Drawer} from "antd";
 import "../App.css";
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser,faBars } from '@fortawesome/free-solid-svg-icons';
+import { faUser,faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux'
 import { logoutResetDetails } from "../redux/actions/userAction"
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from 'react-redux'
-import img from "../image/logo22.png";
+import img from "../image/logoFi.png";
 import navItems from '../config/navItems.json'
 const NavBar = () => {
     const navigate = useNavigate()
@@ -28,12 +28,12 @@ const NavBar = () => {
     return (
         <>
             <div className="navBox1">
-            <div style={{textDecoration:"none", color:"white"}}>
-            {/* <img src={img} alt="Logo" width={200} height={100} margin-bottom={100}/> */}
+            <div className='logo'>
+            <Link to ="/" ><img src={img} alt="Logo" width={200} height={200}  /> </Link>
             </div>
             <Link to="/" style={{color:"#FFFFFF" }}>Home</Link>
             </div>
-            <FontAwesomeIcon icon={faBars}  onClick={showDrawer}className="adminDrawer"></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faArrowRightArrowLeft}  onClick={showDrawer}className="adminDrawer"></FontAwesomeIcon>
 
                 <Drawer
                 title="Admin Panel"
@@ -47,9 +47,10 @@ const NavBar = () => {
 
                 <div className="icon">
                     <div className="user_details">
-                        <Link to="/profile" className="user_name">{name}</Link>
+                     <Link to="/profile" className="user_name">{name}</Link>
                         <button className="button_logout" onClick={() => triggerLogout()}>Logout</button>
                     </div>
+                    
                     <FontAwesomeIcon icon={faUser} className="user_icon" />
                 </div>
                {role==='admin'? <div className="name_admin">Admin</div>:""}
