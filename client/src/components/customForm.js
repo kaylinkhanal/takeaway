d8d6de402603eaf28fe15b2198fd4e19f056274b
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-02-02 20:50:39 +0545

    added draggable marker code

--- client/src/components/customForm.js
+++ client/src/components/customForm.js
@@ -5,37 +5,37 @@ import axios from "axios";
 import {toast} from "react-toastify";
 import { useSelector } from "react-redux";
 import priceMap from "../config/priceMap.json"
-
 import 'react-toastify/dist/ReactToastify.css';
+import Map from "./map"
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
 // toast.configure()
 const CustomForm = (props) => {
   const {_id} =useSelector(state=>state.user)
   const [formStep, setFormStep] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
   const handleBackClick = () => {
-    if (formStep !== 1) {
       setFormStep(formStep - 1);
-    }
   };
 
   toast.success(JSON.stringify(props.orderLists))
-
+ 
   return (
     <Formik
       initialValues={props.orderList || {}}
       onSubmit={async (values, { resetForm }) => {
-          const {weight, unitItems, maxLength} = values
-          const finalPrice= weight* unitItems* maxLength * props.basePrice 
-          setTotalPrice(finalPrice - finalPrice * priceMap[props.categoryName].discountPerUnitPrice)
-        if (formStep === 1) {
+        
+        if (formStep <=2) {
           setFormStep(formStep + 1);
         } else {
           values.senderId = _id
           values.totalPrice = totalPrice
           console.log(values)
-
-          // axios.post(`${process.env.REACT_APP_API_URL}/${props.endpoint}`, values);
+          axios.post(`${process.env.REACT_APP_API_URL}/${props.endpoint}`, values);
         }
+        const {weight, unitItems, maxLength} = values
+        const finalPrice= weight* unitItems* maxLength * props.basePrice 
+        setTotalPrice(finalPrice - finalPrice * priceMap[props.categoryName].discountPerUnitPrice)
       }}
     >
       {({ errors, touched }) => (
@@ -66,34 +66,37 @@ const CustomForm = (props) => {
                 })}
              
               </>
-            ) : (
-              <>
-              {totalPrice}
-                {props.senderDetails.map((item) => {
-                  return (
-                    <div>
-                      <Field
-                        name={item}
-                        key={item}
-                        placeholder={item}
-                        type={item === "password" ? "password" : "text"}
-                      />
-                      {errors[item] && touched[item] ? (
-                        <div className="validaton-message">{errors[item]}</div>
-                      ) : null}
-                    </div>
-                  );
-                })}
-              </>
+            ) : null}
+            {
+              formStep ===2 && (
+               <Map/>
+              )
+            }
+            {formStep ===3  && (
+                <>
+                {totalPrice}
+                  {props.senderDetails.map((item) => {
+                    return (
+                      <div>
+                        <Field
+                          name={item}
+                          key={item}
+                          placeholder={item}
+                          type={item === "password" ? "password" : "text"}
+                        />
+                        {errors[item] && touched[item] ? (
+                          <div className="validaton-message">{errors[item]}</div>
+                        ) : null}
+                      </div>
+                    );
+                  })}
+                </>
             )}
-
-            {formStep >= 2 && (
-              <CustomButton
+                <CustomButton
                 name="Back"
                 onClick={handleBackClick}
                 type="submit"
               />
-            )}
             <CustomButton
               name={formStep === 1 ? "Next" : "Submit"}
               type="submit"
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,12 @@ import { combineReducers } from "redux";
 import logger from 'redux-logger'
 import { persistReducer, persistStore } from 'redux-persist';
 import userSlice from './reducers/userSlice'
+import locationSlice from './reducers/locationSlice'
+
 import storage from 'redux-persist/lib/storage';
 const reducer = combineReducers({
-  user: userSlice
+  user: userSlice,
+  location: locationSlice
 });
 
 const persistConfig = {
