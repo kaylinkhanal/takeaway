94efd8f0b6c4db01a9d9f8412fa308f8c152b0b3
Tulkhatri <tulkhatri01@gmail.com>
2023-02-07 15:37:30 +0545

    Order card design, added style for nav items

--- client/src/components/ordersBox.js
+++ client/src/components/ordersBox.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './button.css'
-import {Button, Popover} from 'antd'
+import '../App.css'
+import { Button, Popover } from 'antd'
 import { AiOutlineSend } from 'react-icons/ai';
 import axios from 'axios'
 import io from 'socket.io-client';
@@ -9,44 +10,51 @@ const socket = io(process.env.REACT_APP_API_URL);
 
 const OrdersBox = (props) => {
    const selectDynamicColor = () => {
-     
-       if(props.item.orderStatus==="Pending"){
-         return 'orange'
-       }else if(props.item.orderStatus==="Accepted"){
+
+      if (props.item.orderStatus === "Pending") {
+         return '#d3c86b'
+      } else if (props.item.orderStatus === "Accepted") {
          return '#4BB543'
-       }else{
+      } else {
          return 'red'
-       }
+      }
    }
 
-   const changeStatus = (status)=>{
+   const changeStatus = (status) => {
       const orderDetails = {
          status: status,
          id: props.item._id
       }
-    socket.emit('orderRequest',orderDetails )
+      socket.emit('orderRequest', orderDetails)
    }
    const content = (
       <div style={{ display: 'flex', alignItems: 'center' }}>
-       <input placeholder="reasons for rejection" />{<AiOutlineSend size={30} marginLeft={8} />}
+         <input placeholder="reasons for rejection" />{<AiOutlineSend size={30} marginLeft={8} />}
       </div>
-    );
+   );
    return (
       <>
-         <div className='orderCategory' id={'adminCardTheme'} style={{backgroundColor: selectDynamicColor()}}>
-         {props.item._id}<br/>
+         <div className='orderCategory' id={'adminCardTheme'} style={{ backgroundColor: selectDynamicColor() }}>
+            <div className='categoryName'>
+               <div>{"ID: " + props.item._id}</div>
+               <div>{"Name: " + props.item.receiverName}</div>
+               <div>{"Phone Number: " + props.item.receiverPhoneNo}</div>
+            </div>
+            <div className='categoryName'>
 
-         {props.item.unitItems}
-         {props.item.weight}
-         {props.item.pickupDate}
-         {props.item.receiverPhoneNo}
-         {props.item.pickupTime}
-         <Button onClick={()=>changeStatus('accept')}>Accept</Button>
-         <Popover content={content} >
-            <Button  onClick={()=>changeStatus('reject')} type="primary">Reject</Button>
-         </Popover>
-   
-            <div className='categoryName'>  {props.item.receiverName} </div>
+               <div>{props.item.weight}{props.item.unitItems}</div>
+               <div>{"Pickup Date: " + props.item.pickupDate}</div>
+               <div>{"Pickup Time " + props.item.pickupTime}</div>
+            </div>
+            <div className='categoryName'>
+
+               <div >Status:  <span className='orderStatus'>{props.item.orderStatus}</span></div>
+               <Button onClick={() => changeStatus('accept')}>Accept</Button>
+               <Popover content={content} >
+                  <Button onClick={() => changeStatus('reject')} type="primary">Reject</Button>
+               </Popover>
+            </div>
+
          </div>
       </>
    )
